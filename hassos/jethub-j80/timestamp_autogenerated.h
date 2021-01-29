8896f71f07f3409f8861df7e508415b092518a5f
Vyacheslav Bocharov <devel@lexina.in>
2021-01-29 11:04:30 +0300

    Fix rename armbian -> hassio for j80

--- hassos/jethub-j80/timestamp_autogenerated.h
+++ hassos/jethub-j80/timestamp_autogenerated.h
@@ -1,2 +1,2 @@
 #define U_BOOT_DATE "Jan 29 2021"
-#define U_BOOT_TIME "09:40:54"
+#define U_BOOT_TIME "11:03:55"
--- hassos/jethub-j80/version_autogenerated.h
+++ hassos/jethub-j80/version_autogenerated.h
@@ -1,4 +1,4 @@
-#define PLAIN_VERSION "2015.01-g5f2bca3-dirty"
+#define PLAIN_VERSION "2015.01-geb085aa-dirty"
 #define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
 #define CONFIG_SYSTEM_AS_ROOT ""
 #define CONFIG_AVB2 ""
