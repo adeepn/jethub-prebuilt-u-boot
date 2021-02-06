28a2842162f603f006a64a43f387ce27faa696db
Vyacheslav Bocharov <devel@lexina.in>
2021-02-06 16:48:34 +0300

    j80 universal boot

--- hassos/jethub-j100/timestamp_autogenerated.h
+++ hassos/jethub-j100/timestamp_autogenerated.h
@@ -1,2 +1,2 @@
 #define U_BOOT_DATE "Feb 06 2021"
-#define U_BOOT_TIME "10:20:59"
+#define U_BOOT_TIME "16:48:12"
--- hassos/jethub-j80/timestamp_autogenerated.h
+++ hassos/jethub-j80/timestamp_autogenerated.h
@@ -1,2 +1,2 @@
 #define U_BOOT_DATE "Feb 06 2021"
-#define U_BOOT_TIME "10:30:23"
+#define U_BOOT_TIME "16:48:06"
