2a7a9eb89fca801b923464dc778289696b94ee5b
Vyacheslav Bocharov <devel@lexina.in>
2021-02-06 18:34:42 +0300

    Fix

--- hassos/jethub-j100/timestamp_autogenerated.h
+++ hassos/jethub-j100/timestamp_autogenerated.h
@@ -1,2 +1,2 @@
 #define U_BOOT_DATE "Feb 06 2021"
-#define U_BOOT_TIME "17:16:48"
+#define U_BOOT_TIME "18:33:29"
--- hassos/jethub-j80/timestamp_autogenerated.h
+++ hassos/jethub-j80/timestamp_autogenerated.h
@@ -1,2 +1,2 @@
 #define U_BOOT_DATE "Feb 06 2021"
-#define U_BOOT_TIME "17:16:42"
+#define U_BOOT_TIME "18:33:23"
